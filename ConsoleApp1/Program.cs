0205aa62d742aa232869ffd45c25672aeaa4300b
Michael Roth <RothMich598@GOTVDSB.CA>
2021-02-17 10:34:57 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = ("Michael");
+            string favMovie = ("Jack and Jill");
+
+            string message = ($"Hello {firstName} I see you like {favMovie}, very interesting choice.");
+            Console.WriteLine(message.ToLower());
+            Console.WriteLine(favMovie.Contains ("The"));
+
+            Console.WriteLine("M   M    RRRR ");
+            Console.WriteLine
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
